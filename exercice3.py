ec4b9f1f99cd74a75af9e076497038359923dbfb
Victor <vic.toad.tor@free.fr>
2015-02-18 23:17:14 +0100

    CLEAN refactor usefull functions

--- exercice3.py
+++ exercice3.py
@@ -1,12 +1,2 @@
 import mathlib
-target = 600851475143
-
-prime_decomp = [1]
-primesgen = mathlib.primes(int(target**0.5))
-rest = target
-while rest > 1:
-    nextprime = next(primesgen)
-    while rest % nextprime == 0:
-        rest = rest / nextprime
-        prime_decomp.append(nextprime)
-print(prime_decomp[-1])
+print(mathlib.factorize(600851475143)[-1])
--- mathlib.py
+++ mathlib.py
@@ -5,3 +5,13 @@ def primes(max):
         if all(map(lambda x:i % x != 0, (el for el in seq if el**0.5 < i))):
             yield i
             seq.append(i)
+
+def factorize(integer):
+    factorization = []
+    primesgenerator = primes(int(integer**0.5))
+    while integer > 1:
+        nextprime = next(primesgenerator)
+        while integer % nextprime == 0:
+            integer = integer / nextprime
+            factorization.append(nextprime)
+    return factorization
