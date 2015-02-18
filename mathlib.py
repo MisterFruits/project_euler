5d86cd29040ab87f289c96ba6944015bf8b1faf5
Victor <vic.toad.tor@free.fr>
2015-02-19 00:56:56 +0100

    IMPROVE prime generator

--- mathlib.py
+++ mathlib.py
@@ -1,12 +1,21 @@
-def primes(max):
-    if max <= 2:
-        return
+def _primes():
     seq = []
     yield 2
-    for i in range(3,max,2):
+    i = 3
+    while True:
         if all(map(lambda x:i % x != 0, (el for el in seq if el**0.5 < i))):
             yield i
             seq.append(i)
+        i += 2
+
+def primes(maximum=float('inf'), nb_max=float('inf')):
+    primesgenerator = _primes()
+    nextprime = next(primesgenerator)
+    nb = 0
+    while nb < nb_max and nextprime < maximum:
+        yield nextprime
+        nb += 1
+        nextprime = next(primesgenerator)
 
 def factorize(integer):
     factorization = []
--- test_mathlib.py
+++ test_mathlib.py
@@ -8,6 +8,24 @@ def test_primes():
     assert [el for el in primes(5)] == [2, 3]
     assert [el for el in primes(10)] == [2, 3, 5, 7]
 
+    assert [el for el in primes(maximum=0)] == []
+    assert [el for el in primes(maximum=1)] == []
+    assert [el for el in primes(maximum=2)] == []
+    assert [el for el in primes(maximum=3)] == [2]
+    assert [el for el in primes(maximum=5)] == [2, 3]
+    assert [el for el in primes(maximum=10)] == [2, 3, 5, 7]
+
+    assert len([el for el in primes(nb_max=0)]) == 0
+    assert len([el for el in primes(nb_max=1)]) == 1
+    assert len([el for el in primes(nb_max=2)]) == 2
+    assert len([el for el in primes(nb_max=3)]) == 3
+    assert len([el for el in primes(nb_max=5)]) == 5
+    assert len([el for el in primes(nb_max=10)]) == 10
+
+    assert [el for el in primes(maximum=10, nb_max=2)] == [2, 3]
+    assert [el for el in primes(maximum=3, nb_max=100)] == [2]
+
+
 def test_factorize():
     assert factorize(5) == [5]
     assert factorize(25) == [5, 5]
