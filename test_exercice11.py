e09ae27c22f42d0f66276071e755a58def8cc727
Victor <vic.toad.tor@free.fr>
2015-02-22 10:21:12 +0100

    IMPROVE adj tests

--- test_exercice11.py
+++ test_exercice11.py
@@ -6,5 +6,8 @@ def test_GridTopologie():
   assert gt._are_adjacents(0, 4)
   assert gt._are_adjacents(0, 5)
 
+  assert not gt._are_adjacents(3, 4)
+  assert not gt._are_adjacents(4, 3)
+
 if __name__ == '__main__':
   test_GridTopologie()
